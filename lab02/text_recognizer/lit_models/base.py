efe3ada0e9f5bae3644df45eefbc2f778b283265
ChungYujoyce <joyce.bhps@gmail.com>
2023-04-18 05:39:49 +0000

    fix bug

--- lab02/text_recognizer/lit_models/base.py
+++ lab02/text_recognizer/lit_models/base.py
@@ -6,6 +6,21 @@ OPTIMIZER = "Adam"
 LR = 1e-3
 LOSS = "cross_entropy"
 
+class Accuracy(pl.metrics.Accuracy):
+    """Accuracy Metric with a hack."""
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor) -> None:
+        """
+        Metrics in Pytorch-lightning 1.2+ versions expect preds to be between 0 and 1 else fails with the ValueError:
+        "The `preds` should be probabilities, but values were detected outside of [0,1] range."
+        This is being tracked as a bug in https://github.com/PyTorchLightning/metrics/issues/60.
+        This method just hacks around it by normalizing preds before passing it in.
+        Normalized preds are not necessary for accuracy computation as we just care about argmax().
+        """
+        if preds.min() < 0 or preds.max() > 1:
+            preds = torch.nn.functional.softmax(preds, dim=-1)
+        super().update(preds=preds, target=target)
+
 class BaseLitModel(pl.LightningModule):
     def __init__(self, args, model):
         super().__init__()
@@ -20,9 +35,9 @@ class BaseLitModel(pl.LightningModule):
         if not loss == "transformer":
             self.loss_fn = getattr(torch.nn.functional, loss)
 
-        self.train_acc = pl.metrics.Accuracy()
-        self.val_acc = pl.metrics.Accuracy()
-        self.test_acc = pl.metrics.Accuracy()
+        self.train_acc = Accuracy()
+        self.val_acc = Accuracy()
+        self.test_acc = Accuracy()
 
     @staticmethod
     def add_to_argparse(parser):
@@ -50,7 +65,7 @@ class BaseLitModel(pl.LightningModule):
         x, y = batch
         logits = self(x)
         loss = self.loss_fn(logits, y)
-        self.log("val_loss", loss, , prog_bar=True)
+        self.log("val_loss", loss, prog_bar=True)
         self.val_acc(logits, y)
         self.log("val_acc", self.val_acc, on_step = False, on_epoch = True, prog_bar = True)
 
