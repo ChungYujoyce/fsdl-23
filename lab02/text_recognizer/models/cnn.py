345b276ccc4c8d7db1de8c601c24fb2dffcb0a79
ChungYujoyce <joyce.bhps@gmail.com>
2023-04-19 00:59:08 +0000

    stride conv, bn

--- lab02/text_recognizer/models/cnn.py
+++ lab02/text_recognizer/models/cnn.py
@@ -13,13 +13,21 @@ IMAGE_SIZE = 28
 class ConvBlock(nn.Module):
     def __init__(self, input_channels: int, output_channels: int) -> None:
         super().__init__()
-        self.conv = nn.Conv2d(input_channels, output_channels, kernel_size=3, stride=1, padding=1)
+        self.conv = nn.Conv2d(input_channels, output_channels, kernel_size=3, stride=2, padding=1) #strided convolution
         self.relu = nn.ReLU()
+        
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        c = self.conv(x)
-        r = self.relu(c)
-        return r
+        identity = x
+        out = self.conv(x)
+        out = self.relu(out)
+        # self.conv_ = nn.Conv2d(identity.shape[-1], identity.shape[-1], 1, 1)
+        # identity = self.conv_(identity)
+        # self.bn = nn.BatchNorm2d(identity.shape[-1])
+        # identity = self.bn(identity)
+        # out += identity
+        # out = self.relu(out)
+        return out
 
 class CNN(nn.Module):
     def __init__(self, data_config: Dict[str, Any], args: argparse.Namespace = None) -> None:
@@ -31,13 +39,14 @@ class CNN(nn.Module):
 
         conv_dim = self.args.get("conv_dim", CONV_DIM)
         fc_dim = self.args.get("fc_dim", FC_DIM)
-
         self.conv1 = ConvBlock(input_dims[0], conv_dim)
         self.conv2 = ConvBlock(conv_dim, conv_dim)
         self.dropout = nn.Dropout(0.25)
-        self.max_pool = nn.MaxPool2d(2)
+        #self.max_pool = nn.MaxPool2d(2)
+        self.bn1 = nn.BatchNorm2d(conv_dim)
+        self.bn2 = nn.BatchNorm2d(conv_dim)
 
-        conv_output_size = IMAGE_SIZE // 2
+        conv_output_size = IMAGE_SIZE // 4 # change ouput size according to stride (1,2) (2,4) (3,6)
         fc_input_dim = int(conv_output_size * conv_output_size * conv_dim)
         self.fc1 = nn.Linear(fc_input_dim, fc_dim)
         self.fc2 = nn.Linear(fc_dim, num_classes)
@@ -46,8 +55,10 @@ class CNN(nn.Module):
         B_, C_, H, W = x.shape
         assert H == W == IMAGE_SIZE
         x = self.conv1(x)
+        x = self.bn1(x)
         x = self.conv2(x)
-        x = self.max_pool(x)
+        x = self.bn2(x)
+        #x = self.max_pool(x)
         x = self.dropout(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
