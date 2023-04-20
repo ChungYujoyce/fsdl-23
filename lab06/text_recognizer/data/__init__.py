57705736fb637c8f69c622a650fae7643936bc27
ChungYujoyce <joyce.bhps@gmail.com>
2023-04-20 20:31:52 +0000

    lab6 init

---
+++ lab06/text_recognizer/data/__init__.py
@@ -0,0 +1,10 @@
+from .util import BaseDataset
+from .base_data_module import BaseDataModule
+from .mnist import MNIST
+
+from .emnist import EMNIST
+from .emnist_lines import EMNISTLines
+
+from .emnist_lines2 import EMNISTLines2
+from .iam_lines import IAMLines
+
