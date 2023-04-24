fc4c693fd357a5d777f21a80d430a95e3eba8b79
ChungYujoyce <joyce.bhps@gmail.com>
2023-04-24 18:16:45 +0000

    lab7 init

---
+++ lab07/text_recognizer/models/__init__.py
@@ -0,0 +1,7 @@
+from .mlp import MLP
+from .cnn import CNN
+
+from .line_cnn import LineCNN
+from .line_cnn_lstm import LineCNNLSTM
+from .line_cnn_transformer import LineCNNTransformer
+from .resnet_transformer import ResnetTransformer
